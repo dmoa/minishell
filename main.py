c06861606c884a3a26f8f15742a5690ea18dd803
Stan O <spammyboy@protonmail.com>
2020-08-07 20:05:03 +0200

    setting variables (basic)

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def startup():
 
 def terminal():
 
+    variables = {}
+
 
     while True:
         user_input = input("$ ").lower()
@@ -22,15 +24,33 @@ def terminal():
         if command in ["exit", "die", "toasterbath", "lavabucket"]:
             break
         elif command == "echo":
-            print(user_input[5:])
+            if user_input[5] == "$" and user_input[6:] in variables:
+                print(variables[user_input[6:]])
+            else:
+                print(user_input[5:])
         elif command == "add":
             try:
-                nums = map(int, user_input[4:].split(" "))
+                passing = user_input[4:].split(" ")
+                nums = []
+                for arg in passing:
+                    if arg[0] == "$":
+                        nums.append(variables[arg[1:]])
+                    else:
+                        nums.append(int(arg))
+
                 print(sum(nums))
             except:
                 bad_input(command)
         elif command == "f":
             try:    print(factorial(int(user_input[2:])))
+            except: bad_input(command)
+        elif command == "set":
+            try:
+                passing = user_input[4:].split(" ", maxsplit=1)
+                variable_name = passing[0]
+                value = passing[1] if not passing[1].isnumeric() else int(passing[1])
+                variables[variable_name] = value
+                print("variable set")
             except: bad_input(command)
 
 
@@ -38,7 +58,8 @@ def bad_input(command):
     docs = {
         "echo": "echo string...",
         "add": "add num1 num2 num3...",
-        "f": "f number"
+        "f": "f number",
+        "set": "set variable_name value"
     }
 
     if command in docs:
